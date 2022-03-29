7b3352163b9114d6f84efd1005ca63bf3fbc8812
jhutchison01 <jackhutchison3577@gmail.com>
2022-03-29 13:55:36 +0100

    Added a few comments

--- CMP1903M Assessment 1 Base Code/Program.cs
+++ CMP1903M Assessment 1 Base Code/Program.cs
@@ -11,22 +11,35 @@ namespace CMP1903M_Assessment_1_Base_Code
     {
         static void Main()
         {
-            //Create 'Input' object
-            //Get either manually entered text, or text from a file
+            //Create input object
+            //Create analyse object
+            //Create report object
 
             Input input = new();
+            Analyse analyse = new();
+            Report report = new();
 
+            //Define response string
             string response;
 
+            //Ask the user if they want to enter their own text, or use text in a file
             Console.WriteLine("1. Do you want to enter the text via the keyboard?");
             Console.WriteLine("2.Do you want to read in the text from a file?");
+
+            //Set response string to what the user inputs
             response = Console.ReadLine();
 
-            string text = "";
+            //Define text string, this will be filled with either the users input or the text from a file
+            string text;
+
+
+            List<int> parameters;
 
             if (response == "1")
             {
                 text = input.ManualTextInput();
+                parameters = analyse.AnalyseText(text);
+                report.outputConsole(parameters);
             }
             else if (response == "2")
             {
@@ -34,6 +47,9 @@ namespace CMP1903M_Assessment_1_Base_Code
                 string file = Console.ReadLine();
                 text = input.FileTextInput(file);
                 Console.WriteLine(text);
+
+                parameters = analyse.AnalyseText(text);
+                report.outputConsole(parameters);
             }
             else
             {
@@ -43,18 +59,14 @@ namespace CMP1903M_Assessment_1_Base_Code
             //Create an 'Analyse' object
             //Pass the text input to the 'analyseText' method
 
-            Analyse analyse = new();
-
-            List<int> parameters = analyse.AnalyseText(text);
+            //List<int> parameters = analyse.AnalyseText(text);
 
             //Receive a list of integers back
 
 
             //Report the results of the analysis
 
-            Report report = new();
-
-            report.outputConsole(parameters);
+            //report.outputConsole(parameters);
 
             //TO ADD: Get the frequency of individual letters?
 
